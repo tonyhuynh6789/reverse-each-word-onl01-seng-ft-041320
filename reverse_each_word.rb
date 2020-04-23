408b1b04a73fd38ba420d67d4c5b5a34efe4f19e
Tony Rim <duc_thinh_03@yahoo.com>
2020-04-23 19:12:03 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,6 +6,6 @@ end
 
 def reverse_each_word(string)
   current_array = string.split
-  reversed_array = current_array.map {|n| n.reverse!}
+  reversed_array = current_array.collect {|n| n.reverse!}
   reversed_array.join(" ")
 end 
